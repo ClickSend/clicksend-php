d11646148ee5af4280ac9b9df9cb98ae38e3da2a
Support <Support@ClickSend.com>
2025-09-05 04:27:59 +0000

    New PHP SDK Generated

--- lib/Model/SmsCampaign.php
+++ lib/Model/SmsCampaign.php
@@ -330,7 +330,8 @@ class SmsCampaign implements ModelInterface, ArrayAccess
     /**
      * Sets from
      *
-     * @param string $from Your sender id - more info: https://help.clicksend.com/en/collections/57584-numbers-sender-ids
+     * @param string $from Your sender id - more info: http://help.clicksend.com/SMS/what-is-a-sender-id-or-sender-number.
+     *
      * @return $this
      */
     public function setFrom($from)
